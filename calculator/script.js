8299c0d85f9ed3d0c264f241a4e14f0390e25a23
TikhonShangin <tikhon.shangin@gmail.com>
2022-03-14 22:16:05 +0300

    day3

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+
+function sum(a, b){
+    return a + b;
+}
+
+sum(10, 20);
+// let buttons = document.getElementsByClassName("button");
+// for (let i = 0; i < buttons.length; i++) {
+
+//     console.log(buttons[i].innerHTML);
+//     // buttons[i].onclick = qwre;
+
+
+
+// }
+
+console.log("end");
