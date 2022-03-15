bb145b7223fc2e62e5827251ee829c6e5a36b679
TikhonShangin <tikhon.shangin@gmail.com>
2022-03-15 22:17:09 +0300

    day4

---
+++ day4/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// let numberValue = 1;
+// let stringValue = "1";
+// let boolValue = true;
+
+// console.log(typeof numberValue);
+// console.log(typeof stringValue);
+// console.log(typeof boolValue);
+
+let a, b, c;
+
+a = 1;
+b = 0;
+
+(a, b) = (b, a);
